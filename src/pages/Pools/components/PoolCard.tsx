2d48890e274cfeafb08f7cb1ee53ee864938d607
Thibault Jaillard <thibault.jaillard@gmail.com>
2023-02-27 13:06:07 +0100

    Add typo

--- src/pages/Pools/components/PoolCard.tsx
+++ src/pages/Pools/components/PoolCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import '../Pools.scss';
 import { Card } from 'components';
+import { PoolsUtils } from 'utils';
+
+import '../Pools.scss';
 
 interface IProps {
     id: string;
@@ -10,7 +12,14 @@ interface IProps {
 }
 
 const PoolCard = ({ id, totalDeposit, maxEarning }: IProps) => {
-    return <Card className='pool-card-container'>{totalDeposit}</Card>;
+    return (
+        <Card className='pool-card-container'>
+            <img width={88} height={88} src={PoolsUtils.getLogoFromId(id)} alt={id} />
+            <div className="name-container">
+                <span className="name">{id}</span>
+            </div>
+        </Card>
+    );
 };
 
 export default PoolCard;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+import * as PoolsUtils from './pools';
+
 export { default as I18n } from './i18n';
 export { default as HttpClient } from './http';
 export { default as Firebase } from './firebase';
+
+export { PoolsUtils };
