3cd150093aee75104c0a6c2a15831a8c5997f998
Thibault Jaillard <thibault.jaillard@gmail.com>
2023-06-05 11:34:19 -0400

    Update launch date

--- src/components/BestPrizeCard/BestPrizeCard.tsx
+++ src/components/BestPrizeCard/BestPrizeCard.tsx
@@ -48,7 +48,7 @@ const BestPrizeCard = ({ biggestPrize, countdownTo }: IProps) => {
             <div className='content'>
                 <div className='title-container'>
                     <h3 className='d-flex flex-row'>
-                        LAUNCH JUNE 5TH
+                        LAUNCH JUNE 9TH
                         {/*<CountDown to={new Date(Date.UTC(2023, 4, 3, 12))} />*/}
                     </h3>
                 </div>
